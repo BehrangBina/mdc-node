c2ff98731fe22dc7e3c0d44a2cccef0737951151
Behrang Bina <behrangbina@hotmail.com>
2020-08-15 17:24:03 +1000

    virtual fields and password encryption

--- models/user.js
+++ models/user.js
@@ -35,3 +35,31 @@ const { strict } = require('assert');
         default:[]
     }
  },{timeStamp:true});
+
+
+ // virtual fields 
+ userSchema.virtual('password')
+    .set(function(password){
+        this._password = password
+        this.salt = uuidv1()
+        this.hashed_password = this.encryptPassword(password) 
+    })
+    .get(function(){
+        return this._password
+    })
+
+userSchema.methods = {
+        encryptPassword: function(password){
+            if(!password) return "";
+            try {
+                return crypto
+                .createHmac('sha1',this.salt)
+                .update(password)
+                .diges('hex');
+            } catch (error) {
+                return '';
+            }
+        }
+}
+
+module.exports = mongoose.model('user',userSchema);
