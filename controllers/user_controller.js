b72fbf03a890ea57dd47d60cb1ef726123ee32fb
Behrang Bina <behrangbina@hotmail.com>
2020-08-17 23:00:57 +1000

    adding error handler ot user controller - duplicate email handler

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,11 +1,15 @@
-const User = require('../models/user')
+const User = require('../models/user');
+const {dbErrorHandler} = require('../helpers/db_error_handler');
+
 exports.signup = (req,res) =>{
     console.log('req.body',req.body)
     const user = new User (req.body)
     user.save((err,user)=>{
         if(err){
             return res.status(400)
-                .json({err})
+                .json({
+                    err:dbErrorHandler(err)
+                })
         }
         res.json({user})
     })
