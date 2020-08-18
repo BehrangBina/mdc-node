4821829029ccd74a4a72fecc43089b7c8f1794e3
Behrang Bina <behrangbina@hotmail.com>
2020-08-18 22:17:30 +1000

    Add Sign in

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,22 +1,57 @@
 const User = require('../models/user');
-const {dbErrorHandler} = require('../helpers/db_error_handler');
-const {validationResult } = require('express-validator');
-exports.signup = (req,res) =>{
-        // Finds the validation errors in this request and wraps them in an object with handy functions
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(400).json({ errors: errors.array() });
-        }
-    
-    console.log('req.body',req.body)
-    const user = new User (req.body)
-    user.save((err,user)=>{
-        if(err){
+const { dbErrorHandler } = require('../helpers/db_error_handler');
+const { validationResult } = require('express-validator');
+const jwt = require('jsonwebtoken'); //to generate sign token
+const expressJwt = require('express-jwt'); //authorization check
+const user = require('../models/user');
+const fs = require('fs');
+const { response } = require('express');
+exports.signup = (req, res) => {
+    // Finds the validation errors in this request and wraps them in an object with handy functions
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    console.log('req.body', req.body)
+    const user = new User(req.body)
+    user.save((err, user) => {
+        if (err) {
             return res.status(400)
                 .json({
-                    err:dbErrorHandler(err)
+                    err: dbErrorHandler(err)
                 })
         }
-        res.json({user})
+        res.json({ user })
+    })
+}
+const pk = fs.readFileSync('./pk.pub');
+
+exports.signin = (req, res) => {
+    const { email, password } = req.body;
+    User.findOne({ email }, (err, user) => {
+        if (err || !user) {
+            return res.status(400).json({
+                err: "User With that email does not exist. Pleasae SignUp"
+            });
+        }
+        //if uuser found => user and pwd should match
+
+        // create auth method
+        if (!user.authenticate(password)) {
+            return res.status(401).json({
+                err: ' Email and Password Does not match'
+            })
+        }
+        // create token
+        const token = jwt.sign({ _id: user._id }, pk);
+
+        // persist  token
+        res.cookie('t', token, { expire: new Date() + 9999 });
+
+        // return response with token
+        const { _id, name, email, role } = user;
+        return res.json({ token, user: { _id, name, email, role } });
+
     })
 }
--- router/user_router.js
+++ router/user_router.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router  = express.Router();
 const {userSignupValidation} = require('../validation/user_validation')
-const{signup} = require('../controllers/user_controller')
+const {signup,signin} = require('../controllers/user_controller')
  
 router.post('/signup',userSignupValidation,signup)
-
+router.post('/signin',signin)
 module.exports = router;
